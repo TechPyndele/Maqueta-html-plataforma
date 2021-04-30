a34b19a54d2a6559a52c3c482740e79b6f9f1826
manuel <hbiaser132@gmail.com>
2021-04-30 18:17:01 -0500

    add new profile experts

--- masterclass/index.js
+++ masterclass/index.js
@@ -1,74 +1,23 @@
 const bgMoradoNombreYFoto = {
     idDesktop: document.querySelector('.ml-masterclass-desktop-nombre-y-foto'),
     idMovil: document.querySelector('.ml-masterclass-movil-nombre-y-foto'),
-    urlImagenDesktop: 'https://pyndele.co/wp-content/uploads/Como-ser-un-lider-que-produce-resultados-desktop.jpg',
-    urlImagenMovil: 'https://pyndele.co/wp-content/uploads/Como-ser-un-lider-que-produce-resultados-movil.jpg',
-    nombreMasterClass: '¿Cómo ser un <br>líder que produce <br>resultados?',
-    nombreExperto: 'Javier Gómez',
+    urlImagenDesktop: 'https://pyndele.co/wp-content/uploads/2021/04/La-importancia-de-la-gestion-del-conocimiento-en-tu-empresa-desktop-1.jpg',
+    urlImagenMovil: 'https://pyndele.co/wp-content/uploads/2021/04/La-importancia-de-la-gestion-del-conocimiento-en-tu-empresa-movil-1.jpg',
+    nombreMasterClass: 'La importancia de la <br> gestión del conocimiento<br> en tu empresa',
+    nombreExperto: 'Olga González',
     historia: false
 }
 
-// const videoIntroduccion = {
-//     idDesktop: document.querySelector('.ml-masterclass-desktop-introduccion'),
-//     idMovil: document.querySelector('.ml-masterclass-movil-introduccion'),
-//     paragraph: `
-    
-//     Cuando te preparas para ser un líder empoderante que moviliza las personas hacia resultados, veras que tu negocio o empresa obtiene objetivos de manera más consistente y sostenible en el tiempo, debido a que las personas que te acompañan estarán más comprometidas con su trabajo. En esta masterclass aprenderás acerca de herramientas prácticas para que fortalezcas tus habilidades de liderazgo orientado al logro de resultados individuales y de equipo.
-
-//     `,
-//     videoId: '535901140',
-// }
-
 const videoIntroduccion = {
-    idDesktop: document.querySelector('#ml-masterclass-desktop-introduccion__video'),
-    idMovil: document.querySelector('#ml-masterclass-movil-introduccion__video'),
+    idDesktop: document.querySelector('.ml-masterclass-desktop-introduccion'),
+    idMovil: document.querySelector('.ml-masterclass-movil-introduccion'),
     paragraph: `
-    
-    Cuando te preparas para ser un líder empoderante que moviliza las personas hacia resultados, veras que tu negocio o empresa obtiene objetivos de manera más consistente y sostenible en el tiempo, debido a que las personas que te acompañan estarán más comprometidas con su trabajo. En esta masterclass aprenderás acerca de herramientas prácticas para que fortalezcas tus habilidades de liderazgo orientado al logro de resultados individuales y de equipo.
+
+    El conocimiento es un eje fundamental de cualquier organización, sin embargo, siempre se ha pensado que solo las grandes empresas deben gestionarlo. La verdad es que cualquier negocio o empresa, de cualquier tamaño, debe gestionar el conocimiento para ser más competitivo y perdurable en el tiempo. En esta masterClass aprenderás acerca de la importancia de la gestión del conocimiento y algunos puntos claves para comprender su funcionamiento y aplicación.
 
     `,
-    videoId: '535901140',
+    videoId: '524964392',
 }
-
-const aprenderasDirigido = {
-    idDesktop: document.querySelector('.ml-masterclass-desktop-aprenderas-dirigido'),
-    idMovil: document.querySelector('.ml-masterclass-movil-aprenderas-dirigido'),
-    dirigido: `Emprendedores, empresarios, empleados, independientes, consultores, personas apasionadas por lo negocios.`
-}
-
-const requisitos = {
-    idDesktop: document.querySelector(".ml-masterclass-desktop-requisitos"),
-    idMovil: document.querySelector(".ml-masterclass-movil-requisitos"),
-    paragraph1: `
-        Disponibilidad para aprender de los mejores expertos de industria
-    `,
-    paragraph2: `
-        Disciplina y perseverancia para aprovechar al máximo los contenidos
-    `,
-    paragraph3: `
-        Aplicar los aprendizajes en tu negocio o empresa
-    `,
-    paragraph4: `
-        Apertura a nuevos conocimientos
-    `,
-}
-
-const redesDeExperto = {
-    idDesktop: document.querySelector('.ml-masterclass-desktop-redes-del-expertos'),
-    idMovil: document.querySelector('.ml-masterclass-movil-redes-del-expertos'),
-    imgExpertoDesktop: 'https://pyndele.co/wp-content/uploads/2021/04/Javier-Gomez-desktop.png',
-    imgExpertoMovil: 'https://pyndele.co/wp-content/uploads/2021/04/Javier-Gomez-movil.png',
-    nombreExperto: 'Javier<br> Gómez',
-    paragraph: `
-    Movilizador de la  transformación de personas y equipos por medioa del coaching desde hace más de 5 años. Se enfoca en la creación de nuevos modelos de pensamiento propios de un lider coach. Es ingeniero industrial de profesión, Coach a nivel profesional, Life coaching y PNL. 
-    `,
-    instagram: '#',
-    linkedin: '#',
-    web: '#',
-
-}
-
-
 
 
 
@@ -138,8 +87,11 @@ if (bgMoradoNombreYFoto.idMovil) {
 // renderiza basandose en la constante [videoIntroduccion] el video de introdduccion y parrafo
 if (videoIntroduccion.idDesktop) {
 
+    // <lite-vimeo videoid="${ videoIntroduccion.videoId }" class="ml-masterclass-desktop-introduccion__video">
+    // </lite-vimeo>
     videoIntroduccion.idDesktop
         .innerHTML = `
+
             <iframe class='ml-masterclass-desktop-introduccion__video'
             src="https://player.vimeo.com/video/${videoIntroduccion.videoId}?badge=0&autopause=0&player_id=0&app_id=58479"
             frameborder="0"
@@ -147,277 +99,59 @@ if (videoIntroduccion.idDesktop) {
             <span data-mce-type="bookmark" class="mce_SELRES_start">﻿
             </span>
             </iframe>
+
+            <div class="ml-masterclass-desktop-introduccion__info">
+
+                <div style='display:flex; flex-direction: column;'>
+                
+                    <h3 class='ml-masterclass-desktop-introduccion__info__title'>
+                    
+                    ¿Que aprenderé <br> <span>con esta masterclass?</span> 
+                    
+                    </h3>
+                    
+                    <p class="ml-masterclass-desktop-introduccion__info__paragraph">
+                    
+                    ${videoIntroduccion.paragraph}
+                    
+                    <img src="https://pyndele.co/wp-content/uploads/2021/04/tres-puntos.svg" 
+                    alt="tres puntos" class="ml-masterclass-desktop-introduccion__info__paragraph__tres-puntos">
+                    
+                    </p>
+                
+                </div>
+
+                <div class="ml-masterclass-desktop-introduccion__info__valor"></div>
+
+            </div>
         `
-
-    document.querySelector('.ml-masterclass-desktop-introduccion__info__paragraph')
-        .innerHTML = `${videoIntroduccion.paragraph}`
-
 }
 
 if (videoIntroduccion.idMovil) {
 
+    // <lite-vimeo videoid="${ videoIntroduccion.videoId }" class="ml-masterclass-movil-introduccion__video">
+    // </lite-vimeo>
     videoIntroduccion.idMovil
         .innerHTML = `
-            
-        <iframe class='ml-masterclass-movil-introduccion__video'
-            src="https://player.vimeo.com/video/${videoIntroduccion.videoId}?badge=0&autopause=0&player_id=0&app_id=58479"
-            frameborder="0"
-            allowfullscreen="allowfullscreen">
-            <span data-mce-type="bookmark" class="mce_SELRES_start">﻿
-            </span>
-        </iframe>
-            
-        `
+            <iframe class='ml-masterclass-movil-introduccion__video'
+                src="https://player.vimeo.com/video/${videoIntroduccion.videoId}?badge=0&autopause=0&player_id=0&app_id=58479"
+                frameborder="0"
+                allowfullscreen="allowfullscreen">
+                <span data-mce-type="bookmark" class="mce_SELRES_start">﻿
+                </span>
+            </iframe>
 
-    document.querySelector('.ml-masterclass-movil-introduccion__paragraph')
-        .innerHTML = `${videoIntroduccion.paragraph}`
-
-}
-
-
-
-// renderiza parrafos de "Que aprendere" y "A quien se dirije"
-if (aprenderasDirigido.idDesktop) {
-    aprenderasDirigido.idDesktop
-        .innerHTML = `
-            <div class="ml-masterclass-desktop-aprenderas-dirigido__row">
-
-                <div class="ml-masterclass-desktop-aprenderas-dirigido__row__title">
-                    ¿A quien se dirige
-                    <span>la masterclass?</span>
-                </div>
-
-                <p class="ml-masterclass-desktop-aprenderas-dirigido__row__paragraph"
-                    id="ml-masterclass-desktop-que-aprenderas">
-                    ${aprenderasDirigido.dirigido}
-                </p>
-
-            </div>
-        `
-
-}
-
-if (aprenderasDirigido.idMovil) {
-    aprenderasDirigido.idMovil
-        .innerHTML = `
-
-            <h3 class="ml-masterclass-movil-aprenderas-dirigido__title">
-                ¿A quien se dirige 
-                <span>la masterclass?</span>
+            <h3 class='ml-masterclass-movil-introduccion__title' >
+                ¿Que aprenderé en esta <br> <span> masterclas</span>
             </h3>
 
-            <p class="ml-masterclass-movil-aprenderas-dirigido__paragraph">
-                ${aprenderasDirigido.dirigido}
-            </p>
-        `
-}
-
-
-
-if (requisitos.idDesktop) {
-
-    requisitos.idDesktop
-        .innerHTML = `
-            <h3 class="ml-masterclass-desktop-requisitos__title">Requisitos</h3>
-
-                </div>
-            <div class="ml-masterclass-desktop-requisitos__points">
-
+            <p class="ml-masterclass-movil-introduccion__paragraph">
             
-                <div class='ml-masterclass-desktop-requisitos__points__item' >
-            
-                    <div class='ml-masterclass-desktop-requisitos__points__item__circle'></div>
-
-                    <div class="ml-masterclass-desktop-requisitos__points__item__text"
-                        id="ml-masterclass-desktop-requisitos__points__item-1">
-
-
-                        ${requisitos.paragraph1}
-                    </div>
-
-                </div>
-
-
-
-                <div class='ml-masterclass-desktop-requisitos__points__item' >
-            
-                    <div class='ml-masterclass-desktop-requisitos__points__item__circle'></div>
-
-                    <div class="ml-masterclass-desktop-requisitos__points__item__text"
-                        id="ml-masterclass-desktop-requisitos__points__item-1">
-
-
-                        ${requisitos.paragraph2}
-                    </div>
-
-                </div>
-            </div>
-
-            <div class="ml-masterclass-desktop-requisitos__points">
-
-                
-                <div class='ml-masterclass-desktop-requisitos__points__item' >
-            
-                    <div class='ml-masterclass-desktop-requisitos__points__item__circle'></div>
-
-                    <div class="ml-masterclass-desktop-requisitos__points__item__text"
-                        id="ml-masterclass-desktop-requisitos__points__item-1">
-
-
-                        ${requisitos.paragraph3}
-                    </div>
-
-                </div>
-
-                
-                <div class='ml-masterclass-desktop-requisitos__points__item' >
-            
-                    <div class='ml-masterclass-desktop-requisitos__points__item__circle'></div>
-
-                    <div class="ml-masterclass-desktop-requisitos__points__item__text"
-                        id="ml-masterclass-desktop-requisitos__points__item-1">
-
-
-                        ${requisitos.paragraph4}
-                    </div>
-
-                </div>
-
-            </div>
-        `
-}
-
-if (requisitos.idMovil) {
-
-    requisitos.idMovil
-        .innerHTML = `
-            <div class="ml-masterclass-movil-requisitos__title">Requisitos</div>
-        
-            <div class="ml-masterclass-movil-requisitos__points">
-                <span></span>
-                <p>
-                    ${requisitos.paragraph1}
-    
-                </p>
-            </div>
-    
-            
-            <div class="ml-masterclass-movil-requisitos__points">
-                <span></span>
-                <p>
-                    ${requisitos.paragraph2}
-    
-                </p>
-            </div>
-    
-            
-            <div class="ml-masterclass-movil-requisitos__points">
-                <span></span>
-                <p>
-                    ${requisitos.paragraph3}
-    
-                </p>
-            </div>
-
-            
-            <div class="ml-masterclass-movil-requisitos__points">
-                <span></span>
-                <p>
-                    ${requisitos.paragraph4}
-    
-                </p>
-            </div>
-        `
-}
-
-
-
-// renderiza la zona donde estan las redes del experto
-if (redesDeExperto.idDesktop) {
-
-    redesDeExperto.idDesktop
-        .innerHTML = `
-            <img src="${redesDeExperto.imgExpertoDesktop}" alt="${redesDeExperto.nombreExperto}"
-                class="ml-masterclass-desktop-redes-del-expertos__fotos">
-
-            <div class="ml-masterclass-desktop-redes-del-expertos__nombre">
-                ${redesDeExperto.nombreExperto}
-            </div>
-
-            <div class="ml-masterclass-desktop-redes-del-expertos__redes">
-
-                <p class="ml-masterclass-desktop-redes-del-expertos__redes__paragraph" style='
-                width: 30vw; margin-top: 1vw;
-                '>
-                    ${redesDeExperto.paragraph}
-                </p>
-
-                <div class="ml-masterclass-desktop-redes-del-expertos__redes__paragraph__row">
-
-                    <a href="${redesDeExperto.instagram}">
-                        <img src="https://pyndele.co/wp-content/uploads/2021/04/instagram.svg" alt="instagram" 
-                            class="ml-masterclass-desktop-redes-del-expertos__redes__paragraph__row__items">
-                    </a>
-
-                    <a href="${redesDeExperto.linkedin}">
-                        <img src="https://pyndele.co/wp-content/uploads/2021/04/linkedin.svg" alt="linkedin" 
-                            class="ml-masterclass-desktop-redes-del-expertos__redes__paragraph__row__items">
-                    </a>
-
-                    <a href="${redesDeExperto.web}">
-                        <img src="https://pyndele.co/wp-content/uploads/2021/04/web.svg" alt="web" 
-                            class="ml-masterclass-desktop-redes-del-expertos__redes__paragraph__row__items">
-                    </a>
-
-                    <img src="https://pyndele.co/wp-content/uploads/2021/04/risa-morada-2.svg" alt="risa morada" 
-                        class="ml-masterclass-desktop-redes-del-expertos__redes__paragraph__row__risa-morada"
-                        style='margin-top: -3vw;'
-                        >
-
-                </div>
-
-            </div>
-        `
-}
-
-if (redesDeExperto.idMovil) {
-
-    redesDeExperto.idMovil
-        .innerHTML = `
-            
-            <div class="ml-masterclass-movil-redes-del-expertos__perfil">
-
-                <img src="${redesDeExperto.imgExpertoMovil}" alt="${redesDeExperto.nombreExperto}" 
-                    class="ml-masterclass-movil-redes-del-expertos__perfil__foto">
-
-                <div class="ml-masterclass-movil-redes-del-expertos__perfil__nombre">
-                ${redesDeExperto.nombreExperto}
-                </div>
-            </div>
-
-            <p class="ml-masterclass-movil-redes-del-expertos__paragraph" >
-
-                ${redesDeExperto.paragraph}
-
+                ${videoIntroduccion.paragraph}
 
             </p>
 
-            <div class="ml-masterclass-movil-redes-del-expertos__redes">
+            <div class="ml-masterclass-movil-introduccion__woo"></div>
 
-                <a href="${redesDeExperto.instagram}">
-                    <img src="https://pyndele.co/wp-content/uploads/2021/04/instagram.svg" alt="instagram" class="ml-masterclass-movil-redes-del-expertos__redes__items">
-                </a>
-
-                <a href="${redesDeExperto.linkedin}">
-                    <img src="https://pyndele.co/wp-content/uploads/2021/04/linkedin.svg" alt="linkedin" class="ml-masterclass-movil-redes-del-expertos__redes__items">
-                </a>
-
-                <a href="${redesDeExperto.web}">
-                    <img src="https://pyndele.co/wp-content/uploads/2021/04/web.svg" alt="web" class="ml-masterclass-movil-redes-del-expertos__redes__items">
-                </a>
-
-            </div>
-
-        </div>
         `
 }
